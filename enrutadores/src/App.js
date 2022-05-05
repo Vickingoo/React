0ffc8a1692886b6ac28b1c77830dee2cf577616d
Victor <vazquezgarridovictor@gmail.com>
2022-05-05 20:27:17 +0200

    hasta enrutadores

---
+++ enrutadores/src/App.js
@@ -0,0 +1,44 @@
+import { Routes, Route, Link, Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route path="invoices" element={<Invoices />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Route>
+    </Routes>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      <h1>Welcome to the app!</h1>
+      <nav>
+        <Link to="invoices">Invoices</Link> |{" "}
+        <Link to="dashboard">Dashboard</Link>
+      </nav>
+      <div className="content">
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+function Invoices() {
+  return (
+    <div>
+       <h1>Dashboard</h1>
+      <Link to="Sent">Sent</Link> |{" "}
+      <Link to="Details">Details</Link>
+      <Outlet />
+    </div>
+  );
+}
+
+function Dashboard() {
+  return <h1>Dashboard</h1>;
+}
+
+export default App;
